715452db93addd76b70e1dbbc4b34f75f496dab6
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-05-15 11:54:08 -0400

    Added a name variable that gets updated on the page as the user types their name into the input text box.

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,31 @@ function App() {
 
   const [count, setCount] = useState(0);
 
-  function handleOnChange(){
-    console.log("Event changing.");
+  // keeps track of the state of the name which will be updated as the user makes changes in the input element below
+  const [name, setName] = useState("");
+
+  function handleChange(event){
+
+    // variable that stores what the user has typed
+    let val = event.target.value;
+
+    // variable that stores the type of input that is expected
+    let type = event.target.type;
+
+    // variable that stores the placeholder text
+    let placeholder = event.target.placeholder;
+
+    console.log("handleChange: val = ", val, "type = ", type, "placeholder = ", placeholder);
+    // sets the value of the name to the value of the user's input as they type
+    setName(val);
   }
 
   return (
     <>
       <div className="container">
-        <h1>Hello </h1>
+        <h1>Hello {name} </h1>
         {/* onChange is triggered every time the value of the input changes */}
-        <input onChange={handleOnChange} type="text" placeholder="What's your name?" />
+        <input onChange={handleChange} type="text" placeholder="What's your name?" />
         <button>Submit</button>
       </div>
     </>
